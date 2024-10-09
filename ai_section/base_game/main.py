a961b0c45a4b214956e7e545fa25b0a9e6602b6d
Joshua Bode <joshua.bode@student.manchester.ac.uk>
2024-10-09 14:45:25 +0100

    Skeleton code for essential classes

---
+++ ai_section/base_game/main.py
@@ -0,0 +1,54 @@
+from typing import List, Set, Dict, Tuple, Type
+
+class Game:                          # Game object
+    def __init__(self) -> None:
+        self.deck = full_deck
+        self.community_cards: List[Type[Card]] = []
+        self.players: List[Type[Player]] = []
+        self.actions: List[Type[Action]] = []            # History of actions throughout the game
+        self.call_amount: int = -1        # Minimum amount to call
+        
+    def deal():                      # Cards from deck are assigned, 2 to each player and the 5 community cards are dealt (face down)
+        pass
+
+    def flop():                      # First three community cards are flipped over
+        pass
+
+    def turn(): 
+        pass
+
+    def river():
+        pass
+    
+    def get_pot():
+        pass
+    
+    def evaluate_hands():
+        pass
+
+
+class Player:
+    def __init__(self) -> None:
+        self.stake: bool = None
+        self.hand: Tuple[Type[Card]] = ()
+    
+class Action:
+    def __init__(self) -> None:
+        pass
+        
+class Card:
+    def __init__(self, num: int, suit: str) -> None:
+        self.num = num  # 1-10 is A-10, J: 11, Q:12, K:13
+        self.suit = suit # Character representing the suit
+        self.public: bool = False
+
+    def __repr__(self) -> str:
+        string = str(self.num) + self.suit
+
+        if len(string) < 3:      # Ensures the output is always 3 characters long
+            return "0" + string
+        return string
+        
+
+if __name__ == "__main__":
+    full_deck = List[Type[Card]]
