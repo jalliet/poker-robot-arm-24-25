be39b1d3cb5cad87421c1cdaa9714990b9de2548
ikram-salahudeen <159075313+ikram-salahudeen@users.noreply.github.com>
2024-12-11 15:45:08 +0000

    Modified file

--- kinematics_section/teensy_code/test_teensy/test_teensy.ino
+++ kinematics_section/teensy_code/test_teensy/test_teensy.ino
@@ -1,9 +1,21 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600); // USB is always 12 or 480 Mbit/sec
+  while (!Serial) {
+    // wait for Arduino Serial Monitor to be ready
+  }
+
+
+}
+
+void received_message(const char* message, int length) {
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
 }
