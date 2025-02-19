89a9496479849c67b3e3f5d60894e419e1ed2d59
ikram-salahudeen <159075313+ikram-salahudeen@users.noreply.github.com>
2025-02-19 14:36:17 +0000

    ikrams stuff

--- kinematics_section/protocol.md
+++ kinematics_section/protocol.md
@@ -2,14 +2,19 @@
 
 # PC => MCU
 
-`set_angles <angle0> <angle1> <angle2> <angle3> <relative speed 1 to 100>\n`
-Set the angles of the 4 motors in degrees, in the range -90 to 90.
+`set_angles <angle0> <angle1> <angle2> <angle3> <rel. speed0> <rel. speed1> <rel. speed2> <rel. speed3>\n`
+Set the angles of the 4 motors in degrees (decimal), in the range -90 to 90.
+Set the relative speed from 0 to 1 (decimal)
 
 `suck\n`
 
 `unsuck\n`
 
 `req_status\n`
+
+`req_periodic_status <period in seconds>\n`
+Request status to be sent periodically. Disable if period is 0.
+
 
 # MCU => PC
 
--- kinematics_section/teensy_code/test_teensy/test_teensy.ino
+++ kinematics_section/teensy_code/test_teensy/test_teensy.ino
@@ -1,16 +1,77 @@
 #include <Arduino.h>
 
+constexpr int MOTOR0_PIN = 0,  MOTOR1_PIN = 1,  MOTOR2_PIN = 2, MOTOR3_PIN = 3;
+constexpr int VALVE_MOTOR_PIN = 0, PUMP_MOTOR_PIN = 0;
+
+float current_angle0, current_angle1, current_angle2, current_angle3, current_speed;
+
+bool reporting_status_periodically = false;
+float status_period = 1;
+
 void setup() {
   Serial.begin(9600); // USB is always 12 or 480 Mbit/sec
   while (!Serial) {
     // wait for Arduino Serial Monitor to be ready
   }
 
+  pinMode(MOTOR0_PIN, OUTPUT);
+  pinMode(MOTOR1_PIN, OUTPUT);
+  pinMode(MOTOR2_PIN, OUTPUT);
+  pinMode(MOTOR3_PIN, OUTPUT);
+
+  pinMode(VALVE_MOTOR_PIN, OUTPUT);
+  pinMode(PUMP_MOTOR_PIN, OUTPUT);
+}
+
+void enable_suction() {
 
 }
 
-void received_message(const char* message, int length) {
-    
+void disable_suction() {
+
+}
+
+void report_status() {
+  Serial.println("my_status " + )
+}
+
+void set_angles(
+  float angle0, float angle1, float angle2, float angle3,
+  float speed0, float speed1, float speed2, float speed3) {
+
+}
+
+
+void received_message(const String* messages, int length) {
+  if (length == 1) {
+    if (messages[0] == "suck") {
+      enable_suction();
+    }
+    else if (messages[0] == "suck") {
+      disable_suction();
+    }
+    else if (messages[0] == "req_status") {
+      report_status();
+    }
+  }
+
+  if (length == 2 && messages[0] == "req_periodic_status") {
+    float period = messages[1].toFloat();
+    if (period == 0) {}
+  }
+
+  if (length == 9 && messages[0] == "set_angles") {
+    set_angles(
+      messages[1].toFloat(), 
+      messages[2].toFloat(), 
+      messages[3].toFloat(), 
+      messages[4].toFloat(), 
+      messages[5].toFloat(),
+      messages[6].toFloat(), 
+      messages[7].toFloat(), 
+      messages[8].toFloat());
+  }
+
 }
 
 void loop() {
