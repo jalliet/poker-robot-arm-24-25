e9aa696cbc2d0aeb3a6c04e007e49a3dcb6c6904
Joshua Bode <joshuabode43@gmail.com>
2025-02-26 17:23:00 +0000

    Move venv_setup to be global to the repository

--- venv_setup.sh
+++ venv_setup.sh
@@ -2,14 +2,14 @@
 
 # Create and activate virtual environment
 # Make sure to use a Python version under 3.13
-python -m venv venv
+python3.12 -m venv venv
 source venv/bin/activate
 
 # Upgrade pip
 pip install --upgrade pip
 
 # Install requirements
-pip install -r ../requirements.txt
+pip install -r requirements.txt
 
 echo "Installation complete! To activate the virtual environment, run:"
 echo "source venv/bin/activate."
